d7cb9741d220fa01925a03973b6dd52fd6225bfa
danielcarrasco <dcartrasco3932@gmail.com>
2020-12-10 13:36:34 -0600

    Initial commit

---
+++ src/main/java/com/orderintergrator/grugHubIntergrator/GrugHubIntergratorApplication.java
@@ -0,0 +1,14 @@
+package com.orderintergrator.grugHubIntergrator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrugHubIntergratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrugHubIntergratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderintergrator/grugHubIntergrator/GrugHubIntergratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.orderintergrator.grugHubIntergrator;
+
+import org.testng.annotations.AfterTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class GrugHubIntergratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
